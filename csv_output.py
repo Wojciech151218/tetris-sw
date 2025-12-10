0033464dc187bae5aba94343c4d1656aff89be44
Wojciech151218 <161569516+Wojciech151218@users.noreply.github.com>
2025-12-10 16:32:37 +0100

    Update CSV output handling and UART import

--- csv_output.py
+++ csv_output.py
@@ -2,13 +2,14 @@ from output import Output
 from game import Game
 from square import Square
 import serial
-import UART
+import Adafruit_BBIO.UART as UART
 
 
 class CsvOutput(Output):
     def __init__(self,uart = "UART1" , port ="/dev/ttyS1",baudrate=9600 ):
         UART.setup(uart)
         self.ser = serial.Serial(port = port, baudrate=baudrate)
+        self.ser.close()
         self.ser.open() 
 
     def __del__(self):
@@ -19,11 +20,12 @@ class CsvOutput(Output):
         def get_csv_from_square(square: Square) -> str:
             return f"{square.x};{square.y};{square.color.value[0]}"
 
-        saquares = game.get_all_squares()
+        squares = game.get_all_squares()
 
-        csv = '\n'.join([get_csv_from_square(s) for s in saquares]) + '\0'
+        csv = '\n'.join([get_csv_from_square(s) for s in squares]) + '\0'
+        message = csv.encode('utf-8')
+        self.ser.write(message)
 
-        self.ser.write(csv)
 
 
 
